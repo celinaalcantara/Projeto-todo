f26161eb4cc01b4bc4d59562ac4ea8a1091d26e9
Celina Alcantara <celinaacarmo@gmail.com>
2022-03-27 19:16:56 -0300

    To do list

--- app/src/main/java/com/generation/todo/FormFragment.kt
+++ app/src/main/java/com/generation/todo/FormFragment.kt
@@ -1,32 +1,60 @@
 package com.generation.todo
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import android.widget.ArrayAdapter
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.generation.todo.databinding.FragmentFormBinding
-import com.generation.todo.databinding.FragmentListBinding
+import com.generation.todo.fragment.TimePickerListener
+import com.generation.todo.model.Categoria
+import java.time.LocalDate
 
-class FormFragment : Fragment() {
+class FormFragment : Fragment(), TimePickerListener {
 
     private lateinit var binding: FragmentFormBinding
+    private var mainViewModel: MainViewModel by activityViewModels<>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         binding = FragmentFormBinding.inflate(
-            layoutInflater, container, false
-        )
+            layoutInflater, container, false)
 
-        binding.buttonSalvar.setOnClickListener {
-            findNavController().navigate(R.id.action_formFragment_to_listFragment)
+        mainViewModel.listCategoria()
+
+        mainViewModel.myCategoriaResponse.observe(viewLifecycleOwner) { response ->
+            Log.d("Requisicao", response.body().toString())
+            spinnerCategoria(response.body())
         }
-        
-        return binding.root
+
+        mainViewModel.dataSelecionada.observe(viewLifecycleOwner) { selectedDate ->
+            binding.editData.setText(selectedDate.toString())
+        }
+
     }
+
+    fun spinnerCategoria(categorias: List<Categoria>?) {
+
+        if (categorias != null) {
+            binding.spinnerCategorias.adapter = ArrayAdapter(
+                requireContext(),
+                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
+                categorias)
+        }
+
+    }
+
+    override fun onTimeSelectec(date: LocalDate) {
+        mainViewModel.dataSelecionada.value = date
+    }
+
+
+
 }
+
+
